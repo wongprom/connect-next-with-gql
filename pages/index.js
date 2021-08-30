0216d549e98ed66ad68d5aea8c59aee861133c0c
wongprom <>
2021-08-30 10:04:28 +0200

    Display book data on client

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,10 @@ const GET_BOOKS = gql`
   query GetBooks {
     books {
       title
+      author {
+        name
+        age
+      }
     }
   }
 `;
@@ -39,69 +43,29 @@ export default function Home() {
   console.log('🚀 ~ file: index.js ~ line 30 ~ Home ~ dataUsers', dataUsers);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {dataBooks?.books.map(({ title, author: { name, age } }) => {
+        return (
+          <div className=" flex flex-col  p-20 bg-blue-100 border border-black mb-4 w-full ">
+            <h3 className="text-blue-400 mb-4 text-lg font-bold flex-grow">
+              Book Title: {title}
+            </h3>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <div className="flex flex-col bg-white p-6 rounded-lg shadow-xl ">
+              <h2 className="mb-2 font-bold text-2xl text-gray-600">
+                Auther Info
+              </h2>
+              <p className="text-gray-500">Name: {name}</p>
+              <p className="text-gray-500">Age: {age}</p>
+            </div>
+          </div>
+        );
+      })}
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center"></main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
